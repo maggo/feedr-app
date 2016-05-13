264a47276f040c5ea0636f32515e4dad1eed933a
Sergej Lotz <serglo@serglo-air.local>
2016-05-13 21:00:57 +0200

    Set initional style

--- src/newtab.js
+++ src/newtab.js
@@ -1,16 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector('#dribbble-container');
+  const container = document.querySelector('#container');
   fetch('http://feedr.mar.co.de/dribbble')
     .then((res) => res.json())
     .then((shots) => {
-      console.log(shots);
+      //console.log(shots);
       shots.forEach((shot) => {
-        let el = document.createElement('li');
+        let el = document.createElement('div');
         el.innerHTML = `
 <a style="display: block;" href="${shot.html_url}">
   <img src="${shot.images.hidpi}">
-  <div>${shot.title}</div>
-  <div>${shot.user.name}</div>
+  <p>${shot.title}<br><span>by ${shot.user.name}</span></p>
 </a>
 `;
         container.appendChild(el);
